a8c04e859c2ecdde6f42b2915b32c2e4fa733685
ricardO <sleiph@gmail.com>
2023-01-31 20:57:48 -0300

    só adicionando uma animacao na protagonista

--- Assets/scripts/protagonista.cs
+++ Assets/scripts/protagonista.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class protagonista : MonoBehaviour {
 
-    Rigidbody2D body;
+    private Rigidbody2D body;
+    private SpriteRenderer imagem;
 
     float horizontal;
     float vertical;
+
+    [SerializeField] private List<Sprite> sprites;
 
     [Range(0, 10.0f)][SerializeField] private float velocidade = 3.0f;
 
     void Start() {
         body = GetComponent<Rigidbody2D>(); 
+        imagem = GetComponent<SpriteRenderer>();
+        imagem.sprite = sprites[0];
     }
 
     private void FixedUpdate() {  
         body.velocity = new Vector2(horizontal * velocidade, vertical * velocidade);
+        imagem.sprite = sprites[(horizontal == 0) ? 0 : 1];
+        imagem.flipX = horizontal == 1;
     }
 
     void Update() {
