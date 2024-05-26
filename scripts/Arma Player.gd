545b96b8935b7c9c12d0c43b6fe8388f20ae53cf
tiago <82677798+mexerica@users.noreply.github.com>
2024-05-26 17:18:07 -0300

    componetizando tudo e arumando o tiro

--- scripts/Arma Player.gd
+++ scripts/Arma Player.gd
@@ -3,4 +3,10 @@ extends Area2D
 const SPEED = 300.0
 
 func _process(delta):
-	position.y -= delta * SPEED	
+	position.y -= delta * SPEED
+
+func _on_area_entered(area):
+	queue_free();
+
+func _on_body_entered(body):
+	queue_free();
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,13 +1,25 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+var arma_player = preload("res://cenas/arma_player.tscn"); 
+var arma_especial = preload("res://cenas/arma_especial.tscn");
+@onready var Tiro_pos = $Tiro_pos;
 
 func _physics_process(delta):
 	var directionX = Input.get_axis("ui_left", "ui_right")
 	var directionY = Input.get_axis("ui_up", "ui_down")
 	if directionX: velocity.x = directionX * SPEED
 	else: velocity.x = move_toward(velocity.x, 0, SPEED)
-	
 	if directionY: velocity.y = directionY * SPEED
 	else: velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("shoot"): atirar("normal");
+	if Input.is_action_just_pressed("especial"): atirar("especial");
+
+func atirar(type):
+	var bala = null; 
+	if type == "normal": bala = arma_player.instantiate();
+	else: bala = arma_especial.instantiate();
+	add_child(bala);
+	bala.global_position = Tiro_pos.global_position;
